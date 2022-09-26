79b29472a9bd57fb5c79286b7844a5acc01ca7ca
Alejandro Medina <almpratto@gmail.com>
2022-09-25 22:44:07 -0500

    Mnesajes q te mandan paljuego

--- Assets/Scripts/EventCounter.cs
+++ Assets/Scripts/EventCounter.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EventCounter : MonoBehaviour
 {
     public int counter;
+    public Text mesageCounter;
+    public int messages = 3;
     public string[] dates;
     public UnityEngine.UI.Text currentDate;
 
     public void AddEventCount() => counter++;
 
-    private void Awake()
+    private void OnEnable()
     {
+        mesageCounter.text = "Tienes " + messages + " mensajes nuevos !!";
         ConclsGroup.NewEventSucces += AddEventCount;
+        ConclsGroup.NewEventSucces += MessageCount;
     }
-
+    private void OnDisable()
+    {
+        ConclsGroup.NewEventSucces -= AddEventCount;
+        ConclsGroup.NewEventSucces -= MessageCount;
+    }
     private void Update()
     {
         if(counter >= 0 && counter < 3)
@@ -34,4 +42,20 @@ public class EventCounter : MonoBehaviour
             currentDate.text = dates[3];
         }
     }
+
+    void MessageCount()
+    {
+        messages--;
+        if(messages <= 0)
+        {
+            messages = 3;
+            GameObject.Find("FadePanel").GetComponent<ChangeScene>().Change("03_Minigame");
+        }
+        mesageCounter.text = "Tienes " + messages + " mensajes nuevos !!";
+
+        
+
+    }
+
 }
+
