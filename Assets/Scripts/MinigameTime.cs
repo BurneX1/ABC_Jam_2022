7f40762c856184c8c3dc909bfb3a7d840f1ff7b7
Adrian Blume <adrianblume1005@gmail.com>
2022-09-25 23:10:10 -0500

    minigameTimer

---
+++ Assets/Scripts/MinigameTime.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameTime : MonoBehaviour
+{
+    public float minigameTime = 80;
+    public UnityEngine.UI.Text text;
+
+    private void Update()
+    {
+        minigameTime -= Time.deltaTime;
+        text.text = minigameTime.ToString("00");
+        if (minigameTime <= 0) FindObjectOfType<ChangeScene>().Change("02_MainMenu");
+    }
+}
