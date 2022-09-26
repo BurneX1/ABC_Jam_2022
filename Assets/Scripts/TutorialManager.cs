d9614c5fa31e1a9c633885878e7887012ce84511
Adrian Blume <adrianblume1005@gmail.com>
2022-09-25 19:06:34 -0500

    tuto

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -6,7 +6,33 @@ public class TutorialManager : MonoBehaviour
 {
     public GameObject cuyIntro;
     public bool firstCuyIntro;
+    public GameObject cuyTips;
+    public GameObject toolTip;
+    public GameObject blockPanel;
+    public float tipDuration;
     private bool canReproduce = true;
+
+    private void Start()
+    {
+        StartCoroutine(cuyTip());
+    }
+
+    private IEnumerator cuyTip()
+    {
+        yield return new WaitUntil(() => firstCuyIntro);
+        yield return new WaitUntil(() => cuyIntro.transform.parent.gameObject.activeInHierarchy == false);
+        yield return new WaitForSeconds(1);
+        GameObject.Find("Canvas").GetComponent<TweenScreens>().TweenPopUp(cuyTips.transform);
+        AudioManager.Instance.Play("Theme");
+        GameObject.Find("Canvas").GetComponent<TweenScreens>().TweenPopUp(toolTip.transform);
+        blockPanel.SetActive(true);
+        firstCuyIntro = false;
+
+        yield return new WaitForSeconds(tipDuration);
+        GameObject.Find("Canvas").GetComponent<TweenScreens>().HidePopUp(cuyTips.transform);
+        GameObject.Find("Canvas").GetComponent<TweenScreens>().HidePopUp(toolTip.transform);
+        blockPanel.SetActive(false);
+    }
 
     private void Update()
     {
@@ -25,14 +51,15 @@ public class TutorialManager : MonoBehaviour
                 {
                     cuyIntro.SetActive(false);
                     AudioManager.Instance.Stop("Theme");
+                    firstCuyIntro = true;
                 }
             }
             else if(Input.GetMouseButton(0))
             {
                 cuyIntro.SetActive(false);
                 AudioManager.Instance.Stop("Theme");
+                firstCuyIntro = true;
             }
-
         }
     }
 }
